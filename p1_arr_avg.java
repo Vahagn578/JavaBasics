fc52551d1c830b5ac8cb8cb22380254ca6db1b7b
Vahagn578 <58001732+Vahagn578@users.noreply.github.com>
2020-10-12 23:37:57 +0400

    Add files via upload

---
+++ p1_arr_avg.java
@@ -0,0 +1,11 @@
+class Scratch {
+    public static void main(String[] args) {
+        int[] arr = {2,4,1,5,8,2,3,0,9,4};
+        double sum = 0;
+        for (int i=0;i<arr.length;i++){
+            sum += arr[i];
+        }
+        double avg = sum / arr.length;
+        System.out.println(avg);
+    }
+}
